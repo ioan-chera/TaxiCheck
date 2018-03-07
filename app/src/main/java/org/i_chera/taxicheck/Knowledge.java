a87a3ec3868cd5c16034c81fdfc596b621677770
Ioan Chera <ioan_chera@yahoo.com>
2018-03-07 22:57:12 +0200

    Added a Knowledge static class
    This one holds some static data which can be easily observed from
    context and used to extract other data from each line.

---
+++ app/src/main/java/org/i_chera/taxicheck/Knowledge.java
@@ -0,0 +1,40 @@
+package org.i_chera.taxicheck;
+
+/**
+ * Holds some static knowledge
+ */
+
+final class Knowledge
+{
+    /**
+     * These must be sorted to avoid duplicates
+     */
+    static final String sKnownCars[] = {
+            "AUDI",
+            "BMW",
+            "CHEVROLET",
+            "CITROEN",
+            "DACIA",
+            "DCAIA",
+            "DAEWOO",
+            "FIAT",
+            "FORD",
+            "HYUNDAI",
+            "KIA",
+            "KYA",
+            "OPEL",
+            "MAZDA",
+            "MERCEDES",
+            "MERCEDEZ",
+            "MERCESES",
+            "NISSAN",
+            "PEUGEOT",
+            "PROTON",
+            "RENAULT",
+            "RENULT",
+            "SEAT",
+            "SKODA",
+            "VOLKSWAGEN",
+
+    };
+}
